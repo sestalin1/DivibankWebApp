8af5e9ed626dfa798a8ecf2ae134e26799ecda45
Juan de la Cruz <juanleonardo.dr@gmail.com>
2021-09-09 20:06:47 -0400

    Client and Loan front-end generated

--- DivibankWebApp/ClientApp/src/app/app.module.ts
+++ DivibankWebApp/ClientApp/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClientComponent } from './client/client.component';
 import { LoanComponent } from './loan/loan.component';
+import { ClientFormComponent } from './client/client-form/client-form.component';
+import { LoanFormComponent } from './loan/loan-form/loan-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ClientComponent,
-    LoanComponent
+    LoanComponent,
+    ClientFormComponent,
+    LoanFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ DivibankWebApp/ClientApp/src/app/loan/loan-form/loan-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loan-form',
+  templateUrl: './loan-form.component.html',
+  styles: [
+  ]
+})
+export class LoanFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
