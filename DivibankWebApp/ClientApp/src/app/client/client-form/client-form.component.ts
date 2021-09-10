20809521bc268862b7b0eaffe3a3f710573800ef
Juan de la Cruz <juanleonardo.dr@gmail.com>
2021-09-09 22:44:43 -0400

    Client and Loan form method customised

--- DivibankWebApp/ClientApp/src/app/client/client-form/client-form.component.ts
+++ DivibankWebApp/ClientApp/src/app/client/client-form/client-form.component.ts
@@ -1,16 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ClientService } from '../../shared/client.service';
+import { NgForm } from '@angular/forms';
+import { Client } from '../../shared/client.model';
 @Component({
   selector: 'app-client-form',
   templateUrl: './client-form.component.html',
   styles: [
   ]
 })
-export class ClientFormComponent implements OnInit {
-
-  constructor() { }
-
+export class MemberFormComponent implements OnInit {
+  constructor(public service: ClientService) {
+  }
   ngOnInit(): void {
   }
-
+  onSubmit(form: NgForm) {
+    if (this.service.formData.id == 0) //we will use the id as identifier for updating or insertion
+      this.insertRecord(form);
+    else
+      this.updateRecord(form);
+  }
+  insertRecord(form: NgForm) {
+    this.service.postClient().subscribe(
+      res => {
+        this.resetForm(form);
+        this.service.refreshList();
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+  updateRecord(form: NgForm) {
+    this.service.putClient().subscribe(
+      res => {
+        this.resetForm(form);
+        this.service.refreshList();
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+  resetForm(form: NgForm) {
+    form.form.reset();
+    this.service.formData = new Client();
+  }
 }
